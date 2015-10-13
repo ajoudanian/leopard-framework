85d7ce68b26815c30d5f7d9dfc30289ff9584b98
amir ajoudanian <ajoudanian@gmail.com>
2015-10-13 14:19:17 +0330

    add some icon

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,25 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require("gulp-notify");
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     gulp.src('./sass/**/*.scss')
         //.pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(notify("Leopard Framework: Sass Done!"));
 });
 
+gulp.task('copy', function () {
+    gulp.src('./assets/icomoon/fonts/*')
+    .pipe(gulp.dest('./css/fonts/clawicon/'))
+    .pipe(notify("Leopard Framework: Copy Done!"));
+});
+
+
+
 gulp.task('sass:watch', function () {
-    gulp.watch(['./sass/**/*.scss','./assets/**/*.scss'], ['sass']);
+    gulp.watch(['./sass/**/*.scss','./assets/**/*.scss'], ['sass','copy']);
 });
